066f2646d6755a0c5339f95f5869a2488abab3b1
sumit <roy.sumit31@gmail.com>
2020-08-20 20:07:40 +0530

    Updates

---
+++ precache-manifest.ed9ab5c2140fffd1faa0072559c20d49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02bee89f3e97abf682e7cc6d9f98cfb",
+    "url": "/testdemo/index.html"
+  },
+  {
+    "revision": "3d7b8b79acfc4332c911",
+    "url": "/testdemo/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "0c5eb72b2d68a457dbbd",
+    "url": "/testdemo/static/css/main.63c5f571.chunk.css"
+  },
+  {
+    "revision": "3d7b8b79acfc4332c911",
+    "url": "/testdemo/static/js/2.09d5ee0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testdemo/static/js/2.09d5ee0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5eb72b2d68a457dbbd",
+    "url": "/testdemo/static/js/main.e6a6e387.chunk.js"
+  },
+  {
+    "revision": "04ac3d10e087389adcfc",
+    "url": "/testdemo/static/js/runtime-main.61e10176.js"
+  }
+]);
